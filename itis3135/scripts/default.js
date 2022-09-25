2ba8cbc46505490e64acf7a2defd622d3ae77f13
eprince3 <eprince3@uncc.edu>
2022-09-25 17:11:09 -0400

    script test

--- itis3135/scripts/default.js
+++ itis3135/scripts/default.js
@@ -2,10 +2,6 @@
 document.getElementById("validation_link_html").setAttribute("href", "https://validator.w3.org/check?uri=" + location.href);
 document.getElementById("validation_link_css").setAttribute("href", "https://jigsaw.w3.org/css-validator/validator?uri=" + location.href);
 
-function scriptTest(){
-    alert("My script works.");
-}
-
 //Date and time
 let today = new Date();
 const weekdays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
@@ -14,4 +10,10 @@ const month = ["January", "February", "March", "April", "May", "June", "July", "
 function displayDate(){
     document.getElementById("current_date").replaceWith("Today is " + today.getTime() + " on " + weekdays[today.getDay()] +
         ", " + today.getDate) + " " + month[today.getDate()] + ", " + today.getFullYear;
+}
+
+//Generate response to user
+function respond(){
+    document.getElementById("response").replaceWith("Platypus Corp welcomes you, " + document.getElementById("fullname") + 
+        "! We're glad you're doing " + document.getElementById("mood") + "!");
 }
