dcd55dace6d472b44e48128d904b01fb661f84dc
eprince3 <eprince3@uncc.edu>
2022-10-12 14:18:51 -0400

    calculator v1

---
+++ itis3135/scripts/calculator.js
@@ -0,0 +1,8 @@
+//Multipy 2 numbers
+function multiply(){
+    let n1 = document.getElementById("num1").value;
+    let n2 = document.getElementById("num2").value;
+    let product = n1 * n2;
+
+    document.getElementById("multiplyResponse").innerHTML = product;
+}
