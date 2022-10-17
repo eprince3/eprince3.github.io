c0fd7a055f5940bb4b14e95ee860af0a027386de
eprince3 <eprince3@uncc.edu>
2022-10-17 18:45:23 -0400

    calculator overhaul

--- itis3135/scripts/calculator.js
+++ itis3135/scripts/calculator.js
@@ -1,13 +1,62 @@
 //add character to input text
+let focus = 0;
 function addChar(num){
-    document.getElementById("calc_input").innerHTML = num;
+    switch(focus)
+    {
+        case 0:
+            document.getElementById("input_1").value = num;
+            focus++;
+            break;
+        case 1:
+            document.getElementById("input_2").value = num;
+            focus++;
+            break;
+        case 2:
+            document.getElementById("input_3").value = num;
+            focus++;
+            break;
+        default:
+            document.getElementById("input_1").value = num;
+            focus = 1;
+    }
 }
 
-//Multipy 2 numbers
-function multiply(){
-    let n1 = document.getElementById("num1").value;
-    let n2 = document.getElementById("num2").value;
-    let product = n1 * n2;
+//Process answer
+function calculate(){
+    let op = document.getElementById("input_2").value;
+    var num1 = Number(document.getElementById("input_1").value);
+    var num2 = Number(document.getElementById("input_3").value);
+    var res;
 
-    document.getElementById("multiplyResponse").innerHTML = product;
+    switch(op){
+        case('x'):
+            res = num1 * num2;
+            break;
+        case('/'):
+            res = num1 / num2;
+            break;
+        case('-'):
+            res = num1 - num2;
+            break;
+        case('+'):
+            res = num1 + num2;
+            break;
+        default:
+            window.alert("Invalid operator.")
+            break;
+    }
+
+    document.getElementById("ans").value = res;
+}
+
+//clear entries
+function clear(){
+    window.alert("test");
+    /*
+    document.getElementById("input_1").value = 0;
+    document.getElementById("input_2").value = '';
+    document.getElementById("input_3").value = '';    
+    document.getElementById("ans").value = '';
+    focus = 0;
+    */
 }
